324ed8ac6fbe672fa413353b6c8b46ac0797e6f0
Joaquín-oc <joaquin.ocana@utec.edu.pe>
2026-04-13 21:55:03 -0500

    prueba

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
-
+#jijiju
 class Usuario(Base):
     __tablename__ = "usuarios"
     id = Column(Integer, primary_key=True, index=True)
